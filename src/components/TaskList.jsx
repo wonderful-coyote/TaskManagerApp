f3b4150cf7fe60219b91b0aba3eea1cdd3dfc211
KaiserX <wiz0isaac@gmail.com>
2023-06-15 15:10:49 +0100

    edited

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import TaskItem from './TaskItem';
 
-const TaskList = ({
-  tasks,
-  deleteTask,
-  editTask,
-  markTaskAsCompleted,
-  editingTaskId,
-  cancelEdit,
-  updateTask,
-}) => {
+const TaskList = ({ tasks, deleteTask, editTask, markTaskAsCompleted, updateTask, editingTaskId, cancelEdit }) => {
   return (
     <ul className="list-group">
-      {tasks.map((task, index) => (
-        <TaskItem
-          key={task.id}
-          task={task}
-          deleteTask={deleteTask}
-          editTask={editTask}
-          markTaskAsCompleted={markTaskAsCompleted}
-          editingTaskId={editingTaskId}
-          cancelEdit={cancelEdit}
-          updateTask={updateTask}
-          index={index + 1} // Add 1 to index to display numbering correctly
-        />
-      ))}
+      {tasks.length === 0 ? (
+        <li className="list-group-item">No tasks added...</li>
+      ) : (
+        tasks.map((task, index) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            editTask={editTask}
+            markTaskAsCompleted={markTaskAsCompleted}
+            updateTask={updateTask}
+            editingTaskId={editingTaskId}
+            cancelEdit={cancelEdit}
+            index={index + 1}
+          />
+        ))
+      )}
     </ul>
   );
 };
